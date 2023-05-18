a36e0624599e2de3a1045a658ef3a4c638b270b9
roxy2440 <psic_rosy244@hotmail.com>
2023-05-17 21:50:37 -0600

    iniciar commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# LDS 3.4. Utilización de widgets
+ 3.4. Utilización de widgets
+
+## Rosalba Jara 
---
+++ android/app/src/main/kotlin/com/example/ldsw_utilizacion_de_widgets/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ldsw_utilizacion_de_widgets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
